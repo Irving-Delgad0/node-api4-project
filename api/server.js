96226e56d2dd6abab0283231deb4015f7e2c16ec
Irving Delgado <rvngdelgado@gmail.com>
2022-05-12 16:12:44 -0500

    completed

--- api/server.js
+++ api/server.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const Users = require('./users-model')
+const cors = require('cors')
 
 
 const server = express()
 
 server.use(express.json())
+server.use(cors())
 
 
 server.get("/", (req,res) => {
@@ -19,7 +21,12 @@ server.get("api/users", (req,res) => {
 
 server.post('/api/register', (req,res) => {
     const {username, password} = req.body
-    
+    req.end();
+})
+
+server.post('api/login', (req,res) => {
+    const {username, password} = req.body
+    req.end();
 })
 
 module.exports = server
